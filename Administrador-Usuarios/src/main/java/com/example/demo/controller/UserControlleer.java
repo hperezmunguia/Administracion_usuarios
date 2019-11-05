97ff5e09dbac382c5e85155f446420616c767cf8
aperez <hperez@a4sys.com>
2019-11-05 09:16:36 -0600

    HTML creacion. Close #3

---
+++ Administrador-Usuarios/src/main/java/com/example/demo/controller/UserControlleer.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserControlleer {
+	
+	@GetMapping("/")
+	public String index() {
+		return"index";
+	}
+	@GetMapping("/user-form")
+	public String getUserForm() {
+		return "user-form/user-view";
+	}
+
+}
